80b90480c82292279c4d4d677bf9863b091ec219
Muhammed Unlu <muhammedunlu40@gmail.com>
2019-09-11 14:20:34 +0300

    DAL Çalışma

---
+++ MyBlog.DAL/Config/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Config
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.CategoryName)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.Property(x => x.Description)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ MyBlog.DAL/Contexts/MyBlogContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.DAL.Config;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Contexts
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options): base(options)
+        {
+            //Veri tabanında oluşturulduğundan ve güncel olduğundan emin ol!
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+
+        public DbSet<User> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<User> Posts { get; set; }
+
+
+    }
+}
