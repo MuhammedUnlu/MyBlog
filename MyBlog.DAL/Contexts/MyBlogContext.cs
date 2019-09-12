4cb6d784803853d76e3da312dd8314b84dae5809
muhammed ünlü <muhammedunlu40@hotmail.com>
2019-09-12 12:15:32 +0300

    Common eklendi

--- MyBlog.DAL/Contexts/MyBlogContext.cs
+++ MyBlog.DAL/Contexts/MyBlogContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Common.Security;
 using MyBlog.DAL.Config;
 using MyBlog.Domain.Entities;
+using MyBlog.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +15,7 @@ namespace MyBlog.DAL.Contexts
         {
             //Veri tabanında oluşturulduğundan ve güncel olduğundan emin ol!
             Database.EnsureCreated();
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -20,6 +23,15 @@ namespace MyBlog.DAL.Contexts
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new PostConfiguration());
             modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 1,
+                UserName = "muhammedunlu40@gmail.com",
+                Email = "muhammedunlu40@gmail.com",
+                PasswordHash = HashHelper.HashPassword("Ankara1."),
+                UserType = UserType.Admin
+            });
         }
 
         public DbSet<User> Categories { get; set; }
