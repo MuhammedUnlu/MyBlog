b2ca6a224f69297690bdc07742d44a58c06d1df7
muhammed ünlü <muhammedunlu40@hotmail.com>
2019-09-12 11:46:50 +0300

    DAL Tamamlandı

---
+++ MyBlog.DAL/UntiOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.DAL.Reposiories;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.UntiOfWork
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
+    {
+        TContext DbContext { get; }
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity;
+
+        int SaveChanges();
+    }
+}
